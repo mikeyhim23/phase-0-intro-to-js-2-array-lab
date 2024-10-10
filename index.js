866c429a42e8f7cfab67c75280bf3655d120c42c
christopher karanja nganga <christopher.nganga@student.moringaschool.com>
2024-10-10 11:42:50 +0300

    Arrays

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Write your solution here!
+const cats  = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendCat(Ralph) {
+    cats.push(Ralph)
+}
+
+function destructivelyPrependCat(Bob) {
+    cats.unshift(Bob)
+}
+
+function destructivelyRemoveLastCat(Garfield) {
+    cats.pop(Garfield)
+}
+
+function destructivelyRemoveFirstCat(Bob) {
+    cats.shift(Bob)
+}
+
+function appendCat(Broom) {
+    return [...cats, Broom];
+}
+
+function prependCat(Arnold) {
+    return [Arnold, ...cats];
+}
+
+function removeLastCat(Garfield) {
+    return cats.slice(0, -1);
+}
+
+function removeFirstCat(Milo) {
+    return cats.slice(1)
+}
